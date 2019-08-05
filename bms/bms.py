e902b91fcd1dd61489bbb039b5e37bb7c0cef0db
Michael Johns <michaelwaynejohns@gmail.com>
2019-08-04 21:57:38 -0500

    Temporarily comment out evcc control

--- bms/bms.py
+++ bms/bms.py
@@ -35,11 +35,11 @@ def main():
             v_av = batt.get_average_voltage()
             id_str = 'Group %2d: t=%.01f (%.01f) v=%.03f (%.03f)' % (g, t, t_av, v, v_av)
             log_print(id_str)
-            if err_str or t_av > 50.0 or v_av > 8.0 or v_av < 6.0:
-                if not err_str:
-                    evcc.off()
-                    err_str = id_str
-                log_print('ERROR: disabled EVCC because of %s' % err_str)
+            #~ if err_str or t_av > 50.0 or v_av > 8.0 or v_av < 6.0:
+                #~ if not err_str:
+                    #~ evcc.off()
+                    #~ err_str = id_str
+                #~ log_print('ERROR: disabled EVCC because of %s' % err_str)
 
 if __name__== '__main__':
     main()
