f83df73f84c081f645b7e6afd4a9c78d46001bd9
Michael Johns <michaelwaynejohns@gmail.com>
2020-08-25 21:40:35 -0500

    Add LEDs to beetle webui

--- html_beetle/index.php
+++ html_beetle/index.php
@@ -23,6 +23,20 @@
                         }
                     }
                 }
+                var elems = document.getElementsByClassName('led');
+                for (var i = 0; i < elems.length; i++) {
+                    var elem = elems[i];
+                    var ts = Date.parse(state[elem.id]['ts']);
+                    var timeout = 1000.0 * parseFloat(state[elem.id]['timeout']);
+                    if (timeout > 0.0 && now > ts + timeout) {
+                        elem.style.backgroundColor = 'red';
+                    } else if (state[elem.id]['value'] == 'enabled' ||
+                             state[elem.id]['value'] == '1') {
+                        elem.style.backgroundColor = 'green';
+                    } else {
+                        elem.style.backgroundColor = 'grey';
+                    }
+                }
             }
 
             function refresh() {
@@ -60,6 +74,16 @@
     </head>
 
     <body onload='init()' style='font-family:arial;font-weight:bold;color:silver;background-color:black;zoom:400%;'>
+
+        <div style='position:absolute;top:10px;right:10px;'>
+            AC <span class='led' id='ac_present' style='height:12px;width:12px;background-color:green;border-radius:50%;display:inline-block;'></span>
+        </div>
+        <div style='position:absolute;top:35px;right:10px;'>
+            Charging <span class='led' id='charging' style='height:12px;width:12px;background-color:green;border-radius:50%;display:inline-block;'></span>
+        </div>
+        <div style='position:absolute;top:60px;right:10px;'>
+            DCDC <span class='led' id='dcdc' style='height:12px;width:12px;background-color:green;border-radius:50%;display:inline-block;'></span>
+        </div>
 
         <div style='font-size:2.5em;'><a class='updateme' id='v'></a> V</div>
         <div style='font-size:2.5em;'><a class='updateme' id='v_acc_batt'></a> V</div>
